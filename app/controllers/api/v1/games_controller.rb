6768e393f52313b11e5a64a6e447dce5464a51a8
alvaro <4lvaro.up@gmail.com>
2019-11-13 01:24:15 -0300

    ADD: params and routes

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -10,19 +10,33 @@ class Api::V1::GamesController < ApplicationController
     end
 
     def create
-        @game = Game.new(game_params)
-        @game.save
+        game = Game.new(game_params)
+        if game.save
+           render json: {status: 'ok'}
+        else
+            render json: {status:'bad'}
+        end 
+        
     end
 
-    # def create
-    #     @movement = Movement.new(movement_params)
-
-    # end
+    def chosee_move
+        @move = Movement.new(move_params)
+        if @move.save
+            render json: {status:'ok'}
+        else
+            render json: {status: 'bad'}
+        end
+    end 
 
     private
-
-    def game_params()
-        params.require(:game).permit(:player_1_name, :player_2_name, )
+    def set_game
+        @game = Game.find(params[:id])
+      end
+    def game_params
+        params.permit(:player_1_name, :player_2_name)
+    end
+    def move_params
+        params.permit(:name, :movement_id)
     end
     
 end
